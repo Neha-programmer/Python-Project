1ab2ac7ba73b2e08bddbf0d7a9878f740dd3f6f5
Neha <neha.goel.sml@gmail.com>
2020-05-21 21:39:56 +0530

    Seventh Commit

---
+++ Facebooklikegame.py
@@ -0,0 +1,5 @@
+list=["Shah Rukh Khan","Salman Khan","Hrithik Roshan","Aamir Khan","Ranbir Kapoor","Ranveer Singh"]
+import random
+a=random.randint(0,len(list))
+name=input("Enter your name")
+print(name+" looks like "+list[a])
